265b4eae036c2365c3cfac0cf5e8c6cae20f7229
Andrei Stroescu <andrei.g.stroescu@gmail.com>
2019-11-22 11:28:16 +0000

    First commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,138 +1,77 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- */
+import React from 'react';
+import './style.css';
 
-import React, { useEffect, memo } from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
-
-import { useInjectReducer } from 'utils/injectReducer';
-import { useInjectSaga } from 'utils/injectSaga';
-import {
-  makeSelectRepos,
-  makeSelectLoading,
-  makeSelectError,
-} from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
-import AtPrefix from './AtPrefix';
-import CenteredSection from './CenteredSection';
-import Form from './Form';
-import Input from './Input';
-import Section from './Section';
-import messages from './messages';
-import { loadRepos } from '../App/actions';
-import { changeUsername } from './actions';
-import { makeSelectUsername } from './selectors';
-import reducer from './reducer';
-import saga from './saga';
-
-const key = 'home';
-
-export function HomePage({
-  username,
-  loading,
-  error,
-  repos,
-  onSubmitForm,
-  onChangeUsername,
-}) {
-  useInjectReducer({ key, reducer });
-  useInjectSaga({ key, saga });
-
-  useEffect(() => {
-    // When initial state username is not null, submit the form to load repos
-    if (username && username.trim().length > 0) onSubmitForm();
-  }, []);
-
-  const reposListProps = {
-    loading,
-    error,
-    repos,
-  };
-
-  return (
-    <article>
-      <Helmet>
-        <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React.js Boilerplate application homepage"
-        />
-      </Helmet>
-      <div>
-        <CenteredSection>
-          <H2>
-            <FormattedMessage {...messages.startProjectHeader} />
-          </H2>
-          <p>
-            <FormattedMessage {...messages.startProjectMessage} />
-          </p>
-        </CenteredSection>
-        <Section>
-          <H2>
-            <FormattedMessage {...messages.trymeHeader} />
-          </H2>
-          <Form onSubmit={onSubmitForm}>
-            <label htmlFor="username">
-              <FormattedMessage {...messages.trymeMessage} />
-              <AtPrefix>
-                <FormattedMessage {...messages.trymeAtPrefix} />
-              </AtPrefix>
-              <Input
-                id="username"
-                type="text"
-                placeholder="mxstbr"
-                value={username}
-                onChange={onChangeUsername}
-              />
-            </label>
-          </Form>
-          <ReposList {...reposListProps} />
-        </Section>
+const HomePage = () => (
+  <div className="app">
+    <div className="sidebar">
+      <div className="section">
+        <h6 className="section-title">ACCOUNT</h6>
+        <div className="section-item">Home</div>
+        <div className="section-item">Explore</div>
       </div>
-    </article>
-  );
-}
 
-HomePage.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
-  onSubmitForm: PropTypes.func,
-  username: PropTypes.string,
-  onChangeUsername: PropTypes.func,
-};
+      <div className="section">
+        <h6 className="section-title">MY COLLECTION</h6>
+        <div className="section-item">My Mix</div>
+        <div className="section-item">Playlists</div>
+        <div className="section-item">Albums</div>
+        <div className="section-item">Tracks</div>
+        <div className="section-item">Videos</div>
+        <div className="section-item">Artists</div>
+      </div>
 
-const mapStateToProps = createStructuredSelector({
-  repos: makeSelectRepos(),
-  username: makeSelectUsername(),
-  loading: makeSelectLoading(),
-  error: makeSelectError(),
-});
-
-export function mapDispatchToProps(dispatch) {
-  return {
-    onChangeUsername: evt => dispatch(changeUsername(evt.target.value)),
-    onSubmitForm: evt => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-      dispatch(loadRepos());
-    },
-  };
-}
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
+      <div className="section">
+        <h6 className="section-title">MY PLAYLISTS</h6>
+        <div className="section-item">Smoky Dishes</div>
+        <div className="section-item">Country Roads</div>
+        <div className="section-item">Muse</div>
+        <div className="section-item">Wowza</div>
+        <div className="section-item">Covers</div>
+        <div className="section-item">Santana</div>
+      </div>
+    </div>
+    <div className="main-page">
+      <div className="search-bar">
+        <input
+          type="search"
+          name="search"
+          placeholder="Search.."
+          align="right"
+        />
+      </div>
+      <div className="title">Featured</div>
+      <div className="page-category">
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+      </div>
+      <div className="title title-mixes">Mixes For You</div>
+      <div className="page-category">
+        <div className="page-category__page-item item-small" />
+        <div className="page-item item-small" />
+        <div className="page-item item-small" />
+        <div className="page-item item-small" />
+        <div className="page-item item-small" />
+        <div className="page-item item-small" />
+        <div className="page-item item-small" />
+      </div>
+      <div className="title">Recently Played</div>
+      <div className="page-category">
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+        <div className="page-item" />
+      </div>
+    </div>
+  </div>
 );
 
-export default compose(
-  withConnect,
-  memo,
-)(HomePage);
+export default HomePage;
